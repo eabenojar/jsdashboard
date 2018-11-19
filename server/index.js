ae62de4fb3b9f570f9e4f3e9881c74e8d1f7fb93
Eric Abenojar <abenoeric@gmail.com>
2018-11-18 18:18:59 -0600

    Adding more types to rootquery

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+// Create an express app
 const app = express();
-
-app.use("/", (req, res) => res.send("HEfeafeaLLO"));
+// bind express with graphql
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(5000, () => {
   console.log("RUNNING ON SERVER 5000");
